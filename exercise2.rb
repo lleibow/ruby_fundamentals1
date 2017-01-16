3c9a29273cada68fa7ec481a42e121858694eaf5
LAURA LEIBOW <lauraleibow@gmail.com>
2017-01-16 15:15:30 -0500

    problems 2 and 3

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+# Problem 1: Calculating a Good tip
+  #I'll be calculating a tip of 15% by multiplying 55 by 15/100 (15%)#
+    puts 55 * 15/100
+
+#Problem 2: Adding a string and integer#
+  puts "5" + "5"
+
+#Problem 3#
+  puts "This is the result of 45628 multiplied by 7839: #{45628 * 7839}"
